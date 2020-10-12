de0b7407edd9ff8ca290a912ed0ed1783fccc01a
ROHIT <56749920+Rathi2000@users.noreply.github.com>
2020-10-13 02:24:47 +0530

    Create code.js

---
+++ code/code.js
@@ -0,0 +1,10 @@
+document.write("Hello World inside seperate js file");
+document.write("<h1>Heading in document.write in js file</h1>");
+var num=9;
+var name="hey";
+var flag=true;
+
+document.write(num);
+document.write(name);
+document.write(flag);
+alert(name);
